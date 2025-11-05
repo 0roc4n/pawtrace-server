313e4d34c2e41310be6573f7e63cae0eecc654eb
0roc4n <esparragojames@gmail.com>
2025-11-05 18:12:13 +0800

    Update Supabase key handling in server.js to provide a default value if the environment variable is not set

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.json());
 
 // --- Supabase Config ---
 const SUPABASE_URL = "https://kmfpanzbakilqmrrqtjm.supabase.co/rest/v1/pet_gps";
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const SUPABASE_KEY = process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttZnBhbnpiYWtpbHFtcnJxdGptIiwicm9sIjoiYW5vbiIsImlhdCI6MTc1MDIwMDkyMCwiZXhwIjoyMDY1Nzc2OTIwfQ.JTPFmFV3IZfFCTEUHvTPClzwo8aGMt66nNRnRCl1AJw";
+
 
 // --- Allow CORS (so IoT devices and browsers can send data freely) ---
 app.use((req, res, next) => {
