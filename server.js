4f10a312cb462e2918dccbb9dbd0c915312891d2
0roc4n <esparragojames@gmail.com>
2025-11-05 18:38:21 +0800

    Update Supabase key in server.js to reflect new role permissions

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 // --- Supabase Config ---
 const SUPABASE_URL = "https://kmfpanzbakilqmrrqtjm.supabase.co/rest/v1/pet_gps";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttZnBhbnpiYWtpbHFtcnJxdGptIiwicm9sIjoiYW5vbiIsImlhdCI6MTc1MDIwMDkyMCwiZXhwIjoyMDY1Nzc2OTIwfQ.JTPFmFV3IZfFCTEUHvTPClzwo8aGMt66nNRnRCl1AJw";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttZnBhbnpiYWtpbHFtcnJxdGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyMDA5MjAsImV4cCI6MjA2NTc3NjkyMH0.JTPFmFV3IZfFCTEUHvTPClzwo8aGMt66nNRnRCl1AJw";
 
 
 // --- Allow CORS (so IoT devices and browsers can send data freely) ---
